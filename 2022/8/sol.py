98916c5c11b8fcdd01e6cec89e00910fc233576f
Alexander Viklund <alexander.viklund@trioptima.com>
2022-12-09 10:25:35 +0100

    Add day 8 and 9

---
+++ 2022/8/sol.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+import sys
+import re
+import q
+import itertools as it
+import math
+import aocd
+from typing import List, Tuple
+from dataclasses import dataclass
+from collections import defaultdict
+
+
+def transpose(m):
+    """[[1, 2], [3, 4]] -> [[1, 3], [2, 4]]"""
+    return list(map(list, zip(*m)))
+
+
+def get_grid(lines, f=None, sep=None):
+    """ """
+    f = f or (lambda x: x)
+    g1 = [list(map(f, list(l) if sep is None else l.split(sep))) for l in lines]
+    print(g1)
+    return transpose(g1)
+
+
+def print_grid(g):
+    for l in transpose(g):
+        #print(l)
+        print("".join(map(lambda n: "X" if n else ".", l)))
+    print()
+
+
+def zero_index_points(points):
+    return [(x - 1, y - 1) for x, y in points]
+
+
+def points_to_grid(points, default_value=False, point_value=True):
+    max_x = 0
+    max_y = 0
+    for x, y in points:
+        max_x = max(x, max_x)
+        max_y = max(y, max_y)
+
+    grid = [[default_value] * (max_y + 1) for _ in range(max_x + 1)]
+    for x, y in points:
+        grid[x][y] = point_value
+    return grid
+
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    with open(filename) as file:
+        lines = [l.strip('\n') for l in file]
+else:
+    filename = "input"
+    lines = aocd.get_data(year=2022, day=8).split("\n")
+
+print("len(lines)", len(lines))
+ans1 = 0
+ans2 = 0
+
+trees = get_grid(lines, f=int)
+print(lines)
+visible = [[False]*len(trees[0]) for _ in range(len(trees))]
+
+
+for x in range(len(trees)):
+    m = -1
+    for y in range(len(trees[x])):
+        if trees[x][y] > m:
+            visible[x][y] = True
+        m = max(trees[x][y], m)
+            
+
+    m = -1
+    for y in reversed(range(len(trees[x]))):
+        if trees[x][y] > m:
+            m = trees[x][y]
+            visible[x][y] = True
+        m = max(trees[x][y], m)
+
+for y in range(len(trees[0])):
+    m = -1
+    for x in range(len(trees)):
+        if trees[x][y] > m:
+            m = trees[x][y]
+            visible[x][y] = True
+        m = max(trees[x][y], m)
+
+    m = -1
+    for x in reversed(range(len(trees))):
+        if trees[x][y] > m:
+            m = trees[x][y]
+            visible[x][y] = True
+        m = max(trees[x][y], m)
+
+def ss(x0, y0):
+    l = []
+    m = trees[x0][y0]
+    s = 0
+    # print("D")
+    for y in range(y0+1, len(trees[0])):
+        s += 1
+        if trees[x0][y] >= m:
+            break
+    l.append(s)
+    s = 0
+
+    # print("U")
+    for y in range(y0-1, -1, -1):
+        s += 1
+        if trees[x0][y] >= m:
+            break
+    l.append(s)
+    s = 0
+
+    # print("R")
+    for x in range(x0+1, len(trees)):
+        s += 1
+        if trees[x][y0] >= m:
+            break
+    l.append(s)
+    s = 0
+
+    # print("L")
+    for x in range(x0-1, -1, -1):
+        s += 1
+        if trees[x][y0] >= m:
+            break
+    l.append(s)
+    s = 1
+    for v in l:
+        s *= v
+    if s > 8:
+        print(x0,y0, l,s)
+    return s
+    
+for l in visible:
+    for v in l:
+        if v:
+            ans1 += 1
+
+for x in range(len(trees)):
+    for y in range(len(trees)):
+        ans2 = max(ans2, ss(x,y))
+print("1:", ans1)
+print("2:", ans2)
+
+if filename == "input":
+    submit = input("submit?")
+    if 'y' in submit.lower():
+        if ans1 != 0:
+            aocd.submit(ans1, year=2022, day=8, part="a")
+        if ans2 != 0:
+            aocd.submit(ans2, year=2022, day=8, part="b")
+
---
+++ 2022/9/sol.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import sys
+import re
+import q
+import itertools as it
+import math
+import aocd
+from typing import List, Tuple
+from dataclasses import dataclass
+from collections import defaultdict
+
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    with open(filename) as file:
+        lines = [l.strip('\n') for l in file]
+else:
+    filename = "input"
+    lines = aocd.get_data(year=2022, day=9).split("\n")
+
+print("len(lines)", len(lines))
+ans1 = 0
+ans2 = 0
+
+visited = set()
+
+@dataclass
+class Point:
+    x: int = 0
+    y: int = 0
+
+    def step(self, d):
+        if d == "R":
+            self.x += 1
+        if d == "L":
+            self.x -= 1
+        if d == "U":
+            self.y += 1
+        if d == "D":
+            self.y -= 1
+    
+    def follow(self, h: "Point"):
+        if abs(self.x - h.x) <= 1 and abs(self.y-h.y) <= 1:
+            return
+        if self.x == h.x:
+            if self.y < h.y:
+                self.y += 1
+            else:
+                self.y -= 1
+        elif self.y == h.y:
+            if self.x < h.x:
+                self.x += 1
+            else:
+                self.x -= 1
+        else:
+            # diagonal
+            if self.x < h.x and self.y < h.y:
+                self.x += 1
+                self.y += 1
+            elif self.x < h.x and self.y > h.y:
+                self.x += 1
+                self.y -= 1
+            elif self.x > h.x and self.y < h.y:
+                self.x -= 1
+                self.y += 1
+            elif self.x > h.x and self.y > h.y:
+                self.x -= 1
+                self.y -= 1
+
+
+h = Point()
+t = Point()
+
+for move in lines:
+    d, c = move.split()
+    c = int(c)
+    for _ in range(c):
+        h.step(d)
+        t.follow(h)
+        visited.add((t.x, t.y))
+
+ans1 = len(visited)
+
+visited2 = set()
+
+rope = [Point() for _ in range(10)]
+for move in lines:
+    d, c = move.split()
+    c = int(c)
+    for _ in range(c):
+        rope[0].step(d)
+        for i in range(9):
+            rope[i+1].follow(rope[i])
+        t = rope[-1]
+        visited2.add((t.x, t.y))
+
+ans2 = len(visited2)
+
+print("1:", ans1)
+print("2:", ans2)
+
+if filename == "input":
+    submit = input("submit?")
+    if 'y' in submit.lower():
+        if ans1 != 0:
+            aocd.submit(ans1, year=2022, day=9, part="a")
+        if ans2 != 0:
+            aocd.submit(ans2, year=2022, day=9, part="b")
+
