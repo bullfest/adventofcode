fafffa6c38b18c601870e20fa219492ae9dfe2ae
Alexander Viklund <alexander.viklund@trioptima.com>
2023-04-19 12:35:14 +0200

    Misc from old laptop

---
+++ 2015/1/sol.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+import sys
+import re
+import q
+import itertools as it
+import math
+import aocd
+from typing import List, Tuple
+from dataclasses import dataclass
+from collections import defaultdict
+
+
+def transpose(m):
+    """[[1, 2], [3, 4]] -> [[1, 3], [2, 4]]"""
+    return list(map(list, zip(*m)))
+
+
+def get_sections(lines):
+    """Split lines on empty lines"""
+    sections = []
+    section = []
+    for l in lines:
+        if l == "":
+            if section != []:
+                sections.append(section)
+                section = []
+        else:
+            section.append(l)
+    sections.append(section)
+    return sections
+
+def parse_ints(*l):
+    return list(map(int, l))
+
+
+def get_grid(lines, f=None, sep=None):
+    """ """
+    f = f or (lambda x: x)
+    return transpose([list(map(f, l if sep is None else l.split(sep))) for l in lines])
+
+
+def print_grid(g):
+    for l in transpose(g):
+        print(l)
+        # print("".join(map(lambda n: "X" if n > 10 else str(n % 10), l)))
+    print()
+
+
+def zero_index_points(points):
+    return [(x - 1, y - 1) for x, y in points]
+
+
+def points_to_grid(points, default_value=False, point_value=True):
+    max_x = 0
+    max_y = 0
+    for x, y in points:
+        max_x = max(x, max_x)
+        max_y = max(y, max_y)
+
+    grid = [[default_value] * (max_y + 1) for _ in range(max_x + 1)]
+    for x, y in points:
+        grid[x][y] = point_value
+    return grid
+
+
+def neighbours(x, y, diagonal=False):
+    l = []
+    if x > 0:
+        l.append((x - 1, y))
+        if y > 0 and diagonal:
+            l.append((x - 1, y - 1))
+        if y + 1 < max_y and diagonal:
+            l.append((x - 1, y + 1))
+    if y > 0:
+        l.append((x, y - 1))
+
+    if x + 1 < max_x:
+        l.append((x + 1, y))
+        if y > 0 and diagonal:
+            l.append((x + 1, y - 1))
+        if y + 1 < max_y and diagonal:
+            l.append((x + 1, y + 1))
+    if y + 1 < max_y:
+        l.append((x, y + 1))
+    return l
+
+year=2015
+day=1
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    with open(filename) as file:
+        lines = [l.strip('\n') for l in file]
+else:
+    filename = "input"
+    lines = aocd.get_data(year=year, day=day).split("\n")
+
+print("len(lines)", len(lines))
+ans1 = 0
+ans2 = 0
+############
+# SOLUTION #
+############
+
+for i, c in enumerate(lines[0]):
+    match c:
+        case "(":
+            ans1 += 1
+        case ")":
+            ans1 -= 1
+    if ans1 < 0 and ans2 == 0:
+        ans2 = i+1
+
+
+###########
+print("1:", ans1)
+print("2:", ans2)
+
+if filename == "input":
+    submit = input("submit?")
+    if 'y' in submit.lower():
+        if ans1 != 0:
+            aocd.submit(ans1, year=year, day=day, part="a")
+        if ans2 != 0:
+            aocd.submit(ans2, year=year, day=day, part="b")
---
+++ 2022/25/sol.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import sys
+import re
+import q
+import itertools as it
+import math
+import aocd
+from typing import List, Tuple
+from dataclasses import dataclass
+from collections import defaultdict
+
+
+def transpose(m):
+    """[[1, 2], [3, 4]] -> [[1, 3], [2, 4]]"""
+    return list(map(list, zip(*m)))
+
+
+def get_sections(lines):
+    """Split lines on empty lines"""
+    sections = []
+    section = []
+    for l in lines:
+        if l == "":
+            if section != []:
+                sections.append(section)
+                section = []
+        else:
+            section.append(l)
+    sections.append(section)
+    return sections
+
+
+def parse_ints(*l):
+    return list(map(int, l))
+
+
+def get_grid(lines, f=None, sep=None):
+    """ """
+    f = f or (lambda x: x)
+    return transpose([list(map(f, l if sep is None else l.split(sep))) for l in lines])
+
+
+def print_grid(g):
+    for l in transpose(g):
+        print(l)
+        # print("".join(map(lambda n: "X" if n > 10 else str(n % 10), l)))
+    print()
+
+
+def zero_index_points(points):
+    return [(x - 1, y - 1) for x, y in points]
+
+
+def points_to_grid(points, default_value=False, point_value=True):
+    max_x = 0
+    max_y = 0
+    for x, y in points:
+        max_x = max(x, max_x)
+        max_y = max(y, max_y)
+
+    grid = [[default_value] * (max_y + 1) for _ in range(max_x + 1)]
+    for x, y in points:
+        grid[x][y] = point_value
+    return grid
+
+
+def neighbours(x, y, diagonal=False):
+    l = []
+    if x > 0:
+        l.append((x - 1, y))
+        if y > 0 and diagonal:
+            l.append((x - 1, y - 1))
+        if y + 1 < max_y and diagonal:
+            l.append((x - 1, y + 1))
+    if y > 0:
+        l.append((x, y - 1))
+
+    if x + 1 < max_x:
+        l.append((x + 1, y))
+        if y > 0 and diagonal:
+            l.append((x + 1, y - 1))
+        if y + 1 < max_y and diagonal:
+            l.append((x + 1, y + 1))
+    if y + 1 < max_y:
+        l.append((x, y + 1))
+    return l
+
+
+year = 2022
+day = 25
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    with open(filename) as file:
+        lines = [l.strip("\n") for l in file]
+else:
+    filename = "input"
+    lines = aocd.get_data(year=year, day=day).split("\n")
+
+print("len(lines)", len(lines))
+ans1 = 0
+ans2 = 0
+
+
+############
+# SOLUTION #
+############
+
+
+def parse_snafu(s: str):
+    n = 0
+    for c in s:
+        n *= 5
+        n += {"0": 0, "1": 1, "2": 2, "-": -1, "=": -2}[c]
+    return n
+
+
+assert parse_snafu("2=-01") == 976
+
+
+def to_snafu(i: int):
+    s = ""
+    while i:
+        d = ((i + 2) % 5) - 2
+        s += {0: "0", 1: "1", 2: "2", -1: "-", -2: "="}[d]
+        i -= d
+        i //= 5
+    return s[::-1]
+
+
+s = 0
+for l in lines:
+    s += parse_snafu(l)
+
+print(s)
+ans1 = to_snafu(s)
+
+###########
+
+print("1:", ans1)
+
+if filename == "input":
+    submit = input("submit?")
+    if "y" in submit.lower():
+        if ans1 != 0:
+            aocd.submit(ans1, year=year, day=day, part="a")
+        if ans2 != 0:
+            aocd.submit(ans2, year=year, day=day, part="b")
